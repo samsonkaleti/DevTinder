978f86c6fbfe4b90d2584f1a1b7d4e6db619d147
samsonkaleti <kaletishyam@gmail.com>
2025-02-19 00:52:47 +0530

    add user model with validation; implement signup and login endpoints with JWT authentication

--- src/app.js
+++ src/app.js
@@ -1,26 +1,83 @@
 const express = require('express') 
 const connect_DB = require("./config/db")
-const User = require("./models/user"); 
+const User = require("./models/user");  
+const bcrypt = require('bcrypt'); 
+const jwt = require('jsonwebtoken'); 
+const cookieParser = require('cookie-parser');
+
 
 
 const app = express() 
 app.use(express.json())
 
+app.use(cookieParser())
+
 
 
 app.post('/signup', async (req,res) =>{
-    const user = await new User(req.body)
+    const {firstName, lastName, emailID, password,age,bio,location,skills,photoUrl,gender} = req.body
     try{
-        await user.save()
-        res.status(200)
-        res.json({message: 'User registered successfully'})
+        if(emailID){
+            const user = await User.findOne({emailID : emailID})
+            if(user) return res.status(400).json({message: 'User already exists with this email'})
+        } else if(firstName || lastName || password|| emailID){
+            return res.status(400).json({message: 'Please provide all the required fields'})
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const newUser = new User({
+            firstName, lastName, emailID, password : hashedPassword,age,bio,location,skills,
+            photoUrl, gender
+        })
+        await newUser.save()
+        res.status(201).json({message: 'User created successfully'})
+
+
+       
         
     }
     catch(err){
         res.json({message: err.message})
     }
 
-}) 
+})  
+
+
+
+app.post('/login', async(req,res) =>{
+    const {emailID,password} = req.body
+    try{
+        const user = await User.findOne({emailID : emailID}) 
+        if(!user) return res.status(400).json({message: 'User not found'})
+        const validPassword = await bcrypt.compare(password,user.password); 
+    
+        if(!validPassword) return res.status(400).json({message: 'Invalid password'})
+            const token = jwt.sign({_id : user._id}, "Shyam@123", {expiresIn: '1h'}) 
+        res.cookie("token", token) ; 
+        res.json({message: 'Logged in successfully'})
+
+
+
+    }catch{
+        res.json({message: err.message})
+
+
+    }
+},) 
+
+app.get('/profile',async (req,res) =>{
+    const {token} = req.cookies
+    if(!token) return res.status(401).json({message: 'Unauthorized'})
+        try{
+            const payload = jwt.verify(token, "Shyam@123")
+            const user = await User.findOne({_id: payload._id})
+            if(!user) return res.status(400).json({message: 'User not found'})
+            res.status(200).json(user)
+
+        }
+            catch(err){
+                res.json({message: err.message})
+            }
+})
 
 app.get('/user', async(req,res) =>{ 
     const emailID = req.body.emailID  
--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,9 @@ const userSchema = new mongoose.Schema({
     firstName:{
         type : String,
         required : true,
+        minLength : 3,
+        maxLength : 20, 
+
     }, 
     lastName:{
         type : String,
@@ -11,6 +14,9 @@ const userSchema = new mongoose.Schema({
     emailID :{
         type : String,
         required : true,
+        lowercase : true,
+        unique : true,
+
     }, 
     password:{
         type : String,
@@ -18,13 +24,40 @@ const userSchema = new mongoose.Schema({
     }, 
     age :{
         type : Number,
-        required : true,
+        min : 18, 
+        max : 100,
+    
     }, 
     gender :{
         type : String,
-        required : true,
-    }
+        validate(value){
+            if(!['male', 'female'].includes(value))
+                throw new Error('Gender should be either male or female')
+        }
+       
+    
+    },
+    photoUrl :{
+        type : String,
+        default : 'https://res.cloudinary.com/devtinder/image/upload/v1631856566/default_user_photo.png'
+    },
+    bio :{
+        type : String,
+        maxLength : 200,
+    },
+    location :{
+        type : String,
+       
+    },
+    skills :{
+        type : [String],
+       
+    },
 
+},{
+    timestamps: true,
+   
+ 
 }) 
 
 module.exports = mongoose.model('User',userSchema); 
